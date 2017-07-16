974bd6af6133ef432d34b707400f60c6cc82f3b8
Qson <qiubite2013@126.com>
2017-07-16 22:29:55 +0800

    xxx

---
+++ test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by Qson on 2017/7/16.
+//  Copyright © 2017年 Qson11. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by Qson on 2017/7/16.
+//  Copyright © 2017年 Qson11. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
